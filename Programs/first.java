ae263622bbc6e634de35ef60464320304a73ff69
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-04-04 07:24:26 +0530

    Add files via upload

---
+++ Programs/first.java
@@ -0,0 +1,9 @@
+class first{
+    public static void main(String args[]){
+        // System.out.print("hello world");
+        // System.out.print("hello world");
+        for(int i=0;i<6;i++){
+            System.out.println("*");
+        }
+    }
+}
