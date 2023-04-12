f3ce0676d4d8f4a5ab0178dbb29b797d1ae9fdf7
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-04-13 00:05:24 +0530

    Add files via upload

---
+++ Programs/lcm.java
@@ -0,0 +1,24 @@
+package programs;
+import java.util.Scanner;
+
+public class lcm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int hcf=1;
+        for(int i=1;i<a;i++){
+            if(a%i==0 && b%i==0){
+                hcf=i;
+            }
+        }
+
+        System.out.println("hcf is "+hcf);
+        // int c=a*b;
+        int lcm=(a*b)/(hcf);
+        System.out.println("The lcm is : "+lcm);
+    }
+
+    public void lcm() {
+    }
+}
