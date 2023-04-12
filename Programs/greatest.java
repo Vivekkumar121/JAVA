6336fd3a4c9961379dd8ee29e806a71d9bef50f3
VIVEK KUMAR <121549772+Vivekkumar121@users.noreply.github.com>
2023-04-13 00:04:28 +0530

    Add files via upload

---
+++ Programs/greatest.java
@@ -0,0 +1,18 @@
+package programs;
+
+import java.util.*;
+public class greatest {
+    public static void main(String[] args) {
+        
+    
+    int a=4,b=6,c=2;
+
+    if(a>b && a>c){
+        System.out.println("a is greater");
+    }
+    else if(b>a && b>c){
+        System.out.println("b is greater");
+    }
+    else {    System.out.println("C is greater");
+    }
+}}
